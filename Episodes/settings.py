8b9bf98a5768bd261d0d5b0b50f0d680cf1eccab
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-11-23 15:46:50 +0530

    resolve host issue

--- Episodes/settings.py
+++ Episodes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%qufh2ea8!-$%_ctzw=in*d2__i#s*3_mph82!+(3m9g*!%@tt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition

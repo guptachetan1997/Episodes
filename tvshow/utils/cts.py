8c2fc86736b977456ecf77785dcf9fdbecde2004
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-11-23 15:26:51 +0530

    fixing structure

--- tvshow/utils/cts.py
+++ tvshow/utils/cts.py
@@ -57,16 +57,3 @@ def build_training_set():
 	extended_tv_df = pd.read_csv(os.path.join(module_dir, 'extra_train_data.csv'))
 	extended_tv_df = extended_tv_df.append(tv_df)
 	return extended_tv_df
-
-def append_on_delete(show):
-	tv_df = pd.DataFrame(columns=cols+genres)
-	show_genre_list = [0]*28
-	show_genre = show.get_genres
-	length = len(show_genre)
-	for genre in show_genre:
-		show_genre_list[genres.index(genre)] = 1.0/length
-	show_datas = [show.seriesName, show.tvdbID , show.network, int(show.userRating), (float(show.siteRating)**2)*float(show.userRating)]
-	tv_df = tv_df.append(pd.DataFrame([show_datas+show_genre_list], columns=cols+genres))
-	extended_tv_df = pd.read_csv(os.path.join(module_dir, 'extra_train_data.csv'))
-	extended_tv_df = extended_tv_df.append(tv_df)
-	

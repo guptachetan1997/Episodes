9d784a60e526dc8c53d444619f243b56c62bc513
guptachetan1997 <gupta.chetan1997@gmail.com>
2016-11-23 15:29:25 +0530

    fixing error#4

--- tvshow/utils/cts.py
+++ tvshow/utils/cts.py
@@ -42,18 +42,19 @@ genres = [
 module_dir = os.path.dirname(__file__)
 
 def build_training_set():
-	show_data = Show.objects.all()
-
 	tv_df = pd.DataFrame(columns=cols+genres)
-
-	for show in show_data:
-		show_genre_list = [0]*28
-		show_genre = show.get_genres
-		length = len(show_genre)
-		for genre in show_genre:
-			show_genre_list[genres.index(genre)] = 1.0/length
-		show_datas = [show.seriesName, show.tvdbID , show.network, int(show.userRating), (float(show.siteRating)**2)*float(show.userRating)]
-		tv_df = tv_df.append(pd.DataFrame([show_datas+show_genre_list], columns=cols+genres))
-	extended_tv_df = pd.read_csv(os.path.join(module_dir, 'extra_train_data.csv'))
-	extended_tv_df = extended_tv_df.append(tv_df)
-	return extended_tv_df
+	try:
+		show_data = Show.objects.all()
+		for show in show_data:
+			show_genre_list = [0]*28
+			show_genre = show.get_genres
+			length = len(show_genre)
+			for genre in show_genre:
+				show_genre_list[genres.index(genre)] = 1.0/length
+			show_datas = [show.seriesName, show.tvdbID , show.network, int(show.userRating), (float(show.siteRating)**2)*float(show.userRating)]
+			tv_df = tv_df.append(pd.DataFrame([show_datas+show_genre_list], columns=cols+genres))
+		extended_tv_df = pd.read_csv(os.path.join(module_dir, 'extra_train_data.csv'))
+		extended_tv_df = extended_tv_df.append(tv_df)
+		return extended_tv_df
+	except:
+		return tv_df

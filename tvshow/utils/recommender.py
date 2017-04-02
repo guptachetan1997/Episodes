fefb9f2467b7d3f242cf7b1c07a75c1252162b7f
guptachetan1997 <gupta.chetan1997@gmail.com>
2017-04-02 19:02:18 +0530

    dirty fix for scipy error

--- tvshow/utils/recommender.py
+++ tvshow/utils/recommender.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import scale
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.preprocessing import scale
 from .cts import build_training_set
 import os
 from random import shuffle
@@ -12,10 +12,10 @@ def get_recommendations():
 	if train_df is None:
 		return []
 	x_train = train_df.iloc[:, 5:]
-	try:
-		x_train = scale(x_train)
-	except:
-		print("First migrations")
+	# try:
+	# 	x_train = scale(x_train)
+	# except:
+	# 	print("First migrations")
 	y_train = train_df.iloc[:, 3]
 	x_train_labels = train_df.iloc[:, 0]
 
@@ -28,19 +28,19 @@ def get_recommendations():
 	x_target = scale(target_df.iloc[:, 5:])
 	x_target_labels = target_df.iloc[:, 0]
 
-	clf = RandomForestClassifier()
-	clf.fit(x_train,y_train)
+	# clf = RandomForestClassifier()
+	# clf.fit(x_train,y_train)
 
-	y_target = clf.predict(x_target)
-
-	new_df = pd.DataFrame()
-	new_df['seriesName'] = x_target_labels
-	new_df['tvdbID'] = target_df.iloc[:, 1]
-	new_df['PredictedRating'] = y_target
-	new_df['indicator'] = (target_df.iloc[:, 4]/target_df.iloc[:, 3])*new_df['PredictedRating']
-
-	new_df = new_df.sort_values(['indicator'], ascending=False)
-	initial_list = list(new_df.iloc[:4, 1])
-	latter_list =  list(new_df.iloc[5:15, 1])
-	shuffle(latter_list)
-	return list(initial_list + latter_list[:5])
+	# y_target = clf.predict(x_target)
+	#
+	# new_df = pd.DataFrame()
+	# new_df['seriesName'] = x_target_labels
+	# new_df['tvdbID'] = target_df.iloc[:, 1]
+	# new_df['PredictedRating'] = y_target
+	# new_df['indicator'] = (target_df.iloc[:, 4]/target_df.iloc[:, 3])*new_df['PredictedRating']
+	#
+	# new_df = new_df.sort_values(['indicator'], ascending=False)
+	initial_list = list(target_df.iloc[:9, 1])
+	# latter_list =  list(new_df.iloc[5:15, 1])
+	# shuffle(latter_list)
+	return list(initial_list)

96dead71d914972f6096e59f4a82c2ea0061082b
guptachetan1997 <gupta.chetan1997@gmail.com>
2018-01-13 18:55:45 +0530

    dd

--- tvshow/migrations/0002_auto_20170402_1854.py
+++ tvshow/migrations/0002_auto_20170402_1854.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('tvshow', '0001_initial'),
+        ('tvshow'),
     ]
 
     operations = [
